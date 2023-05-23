3bfe1c460b5a7231ec22729c5ae07adbb234d959
Jannes Kasper <jannes.kasper@gmail.com>
2023-05-24 00:07:44 +0200

    Added filter dataset

--- load_food_rec.py
+++ load_food_rec.py
@@ -10,6 +10,7 @@ import plotly.graph_objects as go
 import plotly.express as px
 import tqdm
 import cv2
+import random
 
 TRAIN_ANNOTATIONS_PATH = "/home/jannes/Documents/MasterDelft/Q4/DeepLearning/datasets/food_rec/raw_data/public_training_set_release_2.0/annotations.json"
 TRAIN_IMAGE_DIRECTORY = "/home/jannes/Documents/MasterDelft/Q4/DeepLearning/datasets/food_rec/raw_data/public_training_set_release_2.0/images/"
@@ -20,11 +21,9 @@ VAL_IMAGE_DIRECTORY = "/home/jannes/Documents/MasterDelft/Q4/DeepLearning/datase
 
 class CocoWrapper:
 
-    def __init__(self, train_annotations_path: str, train_img_dir: str, val_annotations_path: str, val_img_dir: str) -> None:
-        self.train_ann_path = train_annotations_path
-        self.val_ann_path = val_annotations_path
-        self.train_img_dir = train_img_dir
-        self.val_img_dir = val_img_dir
+    def __init__(self, annotations_path: str, img_dir: str) -> None:
+        self.annotations_path = annotations_path
+        self.img_dir = img_dir
         
         self.__load()
         self.__fix_data()
@@ -32,23 +31,15 @@ class CocoWrapper:
         self.loaded = False
 
     def __load(self):
-        assert os.path.isfile(self.train_ann_path), f"Provided path for train annotations is invalid!"
-        assert os.path.isfile(self.val_ann_path), f"Provided path for validation annotations is invalid!"
-        assert os.path.isdir(self.train_img_dir), f"Provided path for train images is invalid!"
-        assert os.path.isdir(self.val_img_dir), f"Provided path for validation images is invalid!"
+        assert os.path.isfile(self.annotations_path), f"Provided path for train annotations is invalid!"
+        assert os.path.isdir(self.img_dir), f"Provided path for train images is invalid!"
         
-        self.coco_obj_train = COCO(self.train_ann_path)
-        self.coco_obj_validation = COCO(self.val_ann_path)
+        self.coco_obj = COCO(self.annotations_path)
 
-        self.train_imgs = self.coco_obj_train.loadImgs(self.coco_obj_train.getImgIds())
-        self.train_annotations = self.coco_obj_train.loadAnns(self.coco_obj_train.getAnnIds())
-        self.train_categories = self.coco_obj_train.loadCats(self.coco_obj_train.getCatIds())
-        self.train_categorie_names = [cat["name"] for cat in self.train_categories]
-        
-        self.validation_imgs = self.coco_obj_validation.loadImgs(self.coco_obj_validation.getImgIds())
-        self.validation_annotations = self.coco_obj_validation.loadAnns(self.coco_obj_validation.getAnnIds())
-        self.validation_categories = self.coco_obj_validation.loadCats(self.coco_obj_validation.getCatIds())
-        self.validation_categorie_names = [cat["name"] for cat in self.validation_categories]
+        self.imgs = self.coco_obj.loadImgs(self.coco_obj.getImgIds())
+        self.annotations = self.coco_obj.loadAnns(self.coco_obj.getAnnIds())
+        self.categories = self.coco_obj.loadCats(self.coco_obj.getCatIds())
+        self.categorie_names = [cat["name"] for cat in self.categories]
 
     def fix_uneven_data(annotations: list, out_annotation_file: str, image_dir: str):
         assert os.path.isdir(image_dir), f"Fix annotations: Provided path for images is invalid!"
@@ -63,6 +54,21 @@ class CocoWrapper:
         with open(out_annotation_file, 'w') as f:
             json.dump(annotations, f)
         
+    def filterDataset(self, categories: list):        
+        self.imgs = []
+        self.annotations = []
+        self.categories = []
+        self.categorie_names = []
+    
+        catIds = self.coco_obj.getCatIds(catNms=categories)
+        imgIds = self.coco_obj.getImgIds(catIds=catIds)
+        self.imgs = self.coco_obj.loadImgs(imgIds)
+        self.annotations = self.coco_obj.loadAnns(self.coco_obj.getAnnIds(catIds=catIds))
+        self.categories = self.coco_obj.loadCats(catIds)
+        self.categorie_names = categories
+                
+        random.shuffle(self.imgs)
+
     def getClassName(self, classId, coco_categories):
         for i in range(len(coco_categories)):
             if coco_categories[i]['id']==classId:
@@ -92,7 +98,7 @@ class CocoWrapper:
         #         print(f"Annotations of Image: {i+1}/{len(imgs)}")
         # return masks
         if training:
-            coco = self.coco_obj_train
+            coco = self.coco_obj
         else:
             coco = self.coco_obj_validation
 
@@ -114,44 +120,6 @@ class CocoWrapper:
                 img = 
 
             cnt += batch_size
-
-
-def load_food_recognition_22_dataset(train_annotations_path: str, train_img_dir: str, val_annotations_path: str, val_img_dir: str):
-    assert train_annotations_path is not None and os.path.isfile(train_annotations_path), f"Provided path is not valid!"
-
-    train_coco = COCO(train_annotations_path)
-    with open(train_annotations_path) as f:
-        train_annotations_data = json.load(f)
-
-    with open(val_annotations_path) as f:
-        val_annotations_data = json.load(f)
-
-
-    categories = train_coco.loadCats(train_coco.getCatIds())
-
-
-
-    category_names = [_["name_readable"] for _ in categories]
-
-    # Getting all categoriy with respective to their total images
-    no_images_per_category = {}
-
-    for n, i in enumerate(train_coco.getCatIds()):
-        imgIds = train_coco.getImgIds(catIds=i)
-        label = category_names[n]
-        no_images_per_category[label] = len(imgIds)
-
-    img_info = pd.DataFrame(train_coco.loadImgs(train_coco.getImgIds()))
-    no_images_per_category = OrderedDict(sorted(no_images_per_category.items(), key=lambda x: -1 * x[1]))
-    
-    train_annotations_data = fix_data(train_annotations_data, train_img_dir)
-
-    with open('data/train/new_ann.json', 'w') as f:
-        json.dump(train_annotations_data, f)
-    val_annotations_data = fix_data(val_annotations_data, val_img_dir)
-
-    with open('data/val/new_ann.json', 'w') as f:
-        json.dump(val_annotations_data, f)
 
 # Function for taking a annotation & directiory of images and returning new annoation json with fixed image size info
 def fix_data(annotations, directiory):
@@ -184,11 +152,6 @@ def visualize_datasplit(img_per_cat):
 def show_annotation_examples(train_coco, train_annotations_data, num_images):
     annIds = train_coco.getAnnIds(imgIds=train_annotations_data['images'][num_images]['id'])
     annotations = train_coco.loadAnns(annIds)
-
-    # # load and render the image
-    # plt.imshow(plt.imread(TRAIN_IMAGE_DIRECTIORY + train_annotations_data['images'][num_images]['file_name']))
-    # plt.axis('off')
-    # Render annotations on top of the image
     train_coco.showAnns(annotations)
     plt.show()
 
